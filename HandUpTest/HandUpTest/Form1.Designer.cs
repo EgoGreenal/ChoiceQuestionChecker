8d6dab98c3a3d9b402165f0f5a77b8359b7eaa0c
EgoGreenal <40681454+EgoGreenal@users.noreply.github.com>
2021-04-17 20:06:30 +0800

    Add files via upload

---
+++ HandUpTest/HandUpTest/Form1.Designer.cs
@@ -0,0 +1,418 @@
+﻿namespace HandUpTest
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.button1 = new System.Windows.Forms.Button();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.dataGridView2 = new System.Windows.Forms.DataGridView();
+			this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+			this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.tableLayoutPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+			this.statusStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToOrderColumns = true;
+			this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+			this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowTemplate.Height = 23;
+			this.dataGridView1.Size = new System.Drawing.Size(244, 350);
+			this.dataGridView1.TabIndex = 14;
+			// 
+			// Column1
+			// 
+			this.Column1.HeaderText = "提交者";
+			this.Column1.Name = "Column1";
+			this.Column1.ReadOnly = true;
+			// 
+			// Column2
+			// 
+			this.Column2.HeaderText = "成绩";
+			this.Column2.Name = "Column2";
+			this.Column2.ReadOnly = true;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("宋体", 10F);
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(214, 33);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "选择答题卷和答案";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Controls.Add(this.comboBox1);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.numericUpDown2);
+			this.groupBox1.Controls.Add(this.numericUpDown1);
+			this.groupBox1.Controls.Add(this.listBox1);
+			this.groupBox1.Controls.Add(this.button3);
+			this.groupBox1.Controls.Add(this.button2);
+			this.groupBox1.Location = new System.Drawing.Point(12, 75);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(214, 293);
+			this.groupBox1.TabIndex = 4;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "试题配置";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("宋体", 10F);
+			this.label3.Location = new System.Drawing.Point(61, 110);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(35, 14);
+			this.label3.TabIndex = 11;
+			this.label3.Text = "类型";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "单选 / 判断",
+            "多选 - 无部分分",
+            "多选 - 有部分分"});
+			this.comboBox1.Location = new System.Drawing.Point(102, 106);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(106, 22);
+			this.comboBox1.TabIndex = 12;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("宋体", 10F);
+			this.label2.Location = new System.Drawing.Point(61, 80);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(35, 14);
+			this.label2.TabIndex = 9;
+			this.label2.Text = "数量";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("宋体", 10F);
+			this.label1.Location = new System.Drawing.Point(61, 53);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 14);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "分值";
+			// 
+			// numericUpDown2
+			// 
+			this.numericUpDown2.Font = new System.Drawing.Font("Consolas", 10.5F);
+			this.numericUpDown2.Location = new System.Drawing.Point(102, 77);
+			this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numericUpDown2.Name = "numericUpDown2";
+			this.numericUpDown2.Size = new System.Drawing.Size(106, 24);
+			this.numericUpDown2.TabIndex = 10;
+			this.numericUpDown2.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.DecimalPlaces = 1;
+			this.numericUpDown1.Font = new System.Drawing.Font("Consolas", 10.5F);
+			this.numericUpDown1.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+			this.numericUpDown1.Location = new System.Drawing.Point(102, 50);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(106, 24);
+			this.numericUpDown1.TabIndex = 8;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            65536});
+			// 
+			// listBox1
+			// 
+			this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listBox1.Font = new System.Drawing.Font("宋体", 10F);
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(6, 134);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(202, 147);
+			this.listBox1.TabIndex = 13;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(102, 20);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(106, 24);
+			this.button3.TabIndex = 6;
+			this.button3.Text = "删除末尾";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(6, 20);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(90, 24);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "添加";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 41.84615F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 58.15385F));
+			this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.dataGridView2, 1, 0);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(232, 12);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(599, 356);
+			this.tableLayoutPanel1.TabIndex = 4;
+			// 
+			// dataGridView2
+			// 
+			this.dataGridView2.AllowUserToOrderColumns = true;
+			this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column3,
+            this.Column4,
+            this.Column5});
+			this.dataGridView2.Location = new System.Drawing.Point(253, 3);
+			this.dataGridView2.Name = "dataGridView2";
+			this.dataGridView2.RowTemplate.Height = 23;
+			this.dataGridView2.Size = new System.Drawing.Size(343, 350);
+			this.dataGridView2.TabIndex = 15;
+			// 
+			// Column3
+			// 
+			this.Column3.HeaderText = "题号";
+			this.Column3.Name = "Column3";
+			this.Column3.ReadOnly = true;
+			// 
+			// Column4
+			// 
+			this.Column4.HeaderText = "平均分";
+			this.Column4.Name = "Column4";
+			this.Column4.ReadOnly = true;
+			// 
+			// Column5
+			// 
+			this.Column5.HeaderText = "得分率";
+			this.Column5.Name = "Column5";
+			this.Column5.ReadOnly = true;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Font = new System.Drawing.Font("宋体", 10F);
+			this.checkBox1.Location = new System.Drawing.Point(12, 51);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(96, 18);
+			this.checkBox1.TabIndex = 2;
+			this.checkBox1.Text = "去除IP地址";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1,
+            this.toolStripDropDownButton1,
+            this.toolStripStatusLabel2});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 371);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(843, 22);
+			this.statusStrip1.TabIndex = 5;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// toolStripProgressBar1
+			// 
+			this.toolStripProgressBar1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+			this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+			this.toolStripProgressBar1.Size = new System.Drawing.Size(300, 16);
+			this.toolStripProgressBar1.Step = 1;
+			// 
+			// toolStripDropDownButton1
+			// 
+			this.toolStripDropDownButton1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+			this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+			this.toolStripDropDownButton1.Size = new System.Drawing.Size(0, 17);
+			this.toolStripDropDownButton1.Text = "toolStripDropDownButton1";
+			// 
+			// toolStripStatusLabel1
+			// 
+			this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+			this.toolStripStatusLabel1.Size = new System.Drawing.Size(56, 17);
+			this.toolStripStatusLabel1.Text = "处理进度";
+			// 
+			// toolStripStatusLabel2
+			// 
+			this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+			this.toolStripStatusLabel2.Size = new System.Drawing.Size(422, 17);
+			this.toolStripStatusLabel2.Spring = true;
+			this.toolStripStatusLabel2.Text = "帮助";
+			this.toolStripStatusLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.toolStripStatusLabel2.Click += new System.EventHandler(this.toolStripStatusLabel2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(843, 393);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "测试";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.tableLayoutPanel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.DataGridView dataGridView2;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.NumericUpDown numericUpDown2;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripDropDownButton1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+	}
+}
+
---
+++ HandUpTest/HandUpTest/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Office.Core;
+using MSE = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
+namespace HandUpTest
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		static string Invert(string s,bool fl)
+		{
+			if (!fl) return s;
+			for (int i = s.Length - 1; i >= 0; --i) if (s[i].Equals('_')) return s.Substring(i + 1);
+			return s;
+		}
+		static double Comp(string ans,string ret,bool fl)
+		{
+			int[] c_ans = new int[26], c_ret = new int[26];
+			int allcs = 0, respond = 0;
+			foreach (char i in ans) if (c_ans[i - 'A']++ == 0) ++allcs;
+			foreach (char i in ret) if (i < 'A' || i > 'Z' || c_ret[i - 'A']++ > 0) return 0;
+			for (int i = 0; i < 26; ++i)
+			{
+				if (c_ret[i] > 0 && c_ans[i] == 0) return 0;
+				if (c_ret[i] > 0 && c_ans[i] > 0) ++respond;
+			}
+			return respond == 0 ? 0 : (respond == allcs ? 1 : (fl ? 0.5 : 0));
+		}
+		static bool Check(string ans)
+		{
+			int[] c_ans = new int[26];
+			foreach (char i in ans) if (i < 'A' || i > 'Z' || c_ans[i - 'A']++ > 0) return true;
+			return false;
+		}
+		private void button1_Click(object sender, EventArgs e)
+		{
+			toolStripProgressBar1.Value = 0;
+			openFileDialog1.Title = "选择答案文件";
+			openFileDialog1.Filter = "电子表格|*.xlsx;*.xls";
+			if (openFileDialog1.ShowDialog() != DialogResult.OK)
+			{
+				MessageBox.Show("选取文件失败！", "⚠");
+				return;
+			}
+			if (!(folderBrowserDialog1.ShowDialog() == DialogResult.OK && !string.IsNullOrEmpty(folderBrowserDialog1.SelectedPath)))
+			{
+				MessageBox.Show("选取文件夹失败！", "⚠");
+				return;
+			}
+			string path = openFileDialog1.FileName.ToString();
+			string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+			MSE.Application app = new MSE.Application();
+			MSE.Workbooks wbks = app.Workbooks;
+			MSE._Workbook _wbk = wbks.Add(path);
+			MSE.Sheets shs = _wbk.Sheets;
+			MSE._Worksheet _wsh = (MSE._Worksheet)shs.get_Item(1);
+			string[] answer = new string[General.prob];
+			for (int i = 0; i < General.prob; ++i)
+			{
+				answer[i] = ((MSE.Range)_wsh.Cells[i + 1, 1]).Text.ToString().ToUpper();
+				if (Check(answer[i]))
+				{
+					MessageBox.Show("第 " + (i + 1).ToString() + " 行答案不符合规范", "⚠");
+					return;
+				}
+			}
+			//	_wbk.Close(null, null, null);
+			//	wbks.Close();
+			int sum = 0, index;
+			dataGridView1.AllowUserToAddRows = false;
+			dataGridView2.AllowUserToAddRows = false;
+			double[] anal = new double[General.prob];
+			for (int num = 0; num < files.Length; ++num)
+			{
+				if (!files[num].EndsWith(".xlsx") && !files[num].EndsWith(".xls")) continue;
+				++sum;
+				double scr = 0;
+				_wbk = wbks.Add(files[num]);
+				shs = _wbk.Sheets;
+				_wsh = (MSE._Worksheet)shs.get_Item(1);
+				for (int i = 0; i < General.prob; ++i)
+				{
+					double tempor = Comp(answer[i], ((MSE.Range)_wsh.Cells[i + 1, 1]).Text.ToString().ToUpper(), (General.ps[i] & 1) > 0);
+					scr += tempor * (General.ps[i] >> 1) / 2.0; anal[i] += tempor * (General.ps[i] >> 1) / 2.0;
+				}
+				index = dataGridView1.Rows.Add();
+				dataGridView1.Rows[index].Cells[0].Value = Invert(Path.GetFileNameWithoutExtension(files[num]), checkBox1.Checked);
+				dataGridView1.Rows[index].Cells[1].Value = scr;
+				dataGridView1.FirstDisplayedScrollingRowIndex = index;
+				_wbk.Close(null, null, null);
+				toolStripProgressBar1.Value = (int)Math.Round((num + 1) * 100.0 / files.Length);
+			//	wbks.Close();
+			}
+			MessageBox.Show("共" + sum + "个提交", "ℹ");
+			app.Quit();
+			System.Runtime.InteropServices.Marshal.ReleaseComObject(app);
+			double total = 0, full = 0;
+			for (int i = 0; i < General.prob; ++i)
+			{
+				index = dataGridView2.Rows.Add();
+				dataGridView2.Rows[index].Cells[0].Value = (i + 1).ToString();
+				dataGridView2.Rows[index].Cells[1].Value = anal[i] / sum;
+				dataGridView2.Rows[index].Cells[2].Value = 100 * anal[i] / sum / (General.ps[i] >> 1) * 2;
+				total += anal[i]; full += (General.ps[i] >> 1) / 2.0;
+			}
+			index = dataGridView2.Rows.Add();
+			dataGridView2.Rows[index].Cells[0].Value = "总平均分";
+			dataGridView2.Rows[index].Cells[1].Value = total / sum;
+			dataGridView2.Rows[index].Cells[2].Value = 100 * total / sum / full;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			comboBox1.Text = "单选 / 判断";
+			General.prob = 100;
+			General.all = 3;
+			General.ps.Clear();
+			for (int i = 1; i <= 60; ++i) General.ps.Add(6);
+			listBox1.Items.Add("单选 / 判断 1.5 × 60"); General.prob_count.Add(60);
+			for (int i = 61; i <= 70; ++i) General.ps.Add(12);
+			listBox1.Items.Add("多选 - 无部分分 3.0 × 10"); General.prob_count.Add(10);
+			for (int i = 71; i <= 100; ++i) General.ps.Add(4);
+			listBox1.Items.Add("单选 / 判断 1 × 30"); General.prob_count.Add(30);
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			if (General.all == 0) return;
+			listBox1.Items.RemoveAt(--General.all);
+			General.ps.RemoveRange(General.prob - General.prob_count.Last(), General.prob_count.Last());
+			General.prob -= General.prob_count.Last();
+			General.prob_count.RemoveAt(General.all);
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			decimal tmp = 2 * numericUpDown1.Value, num=numericUpDown2.Value;
+			if (!tmp.Equals(Math.Round(tmp)))
+			{
+				MessageBox.Show("分值最多精确到半分！", "⚠");
+				return;
+			}
+			if (!num.Equals(Math.Round(num)))
+			{
+				MessageBox.Show(num.ToString() + "题目数量必须为整数！", "⚠");
+				return;
+			}
+			++General.all;
+			int ned = (int)(tmp * 2 + (comboBox1.Text == "多选 - 有部分分" ? 1 : 0));
+			for (int i = 0; i < num; ++i) General.ps.Add(ned);
+			listBox1.Items.Add(comboBox1.Text + " " + numericUpDown1.Value.ToString() + " × " + num.ToString());
+			General.prob += (int)num;
+			General.prob_count.Add((int)num);
+		}
+
+		private void toolStripStatusLabel2_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show(" · 成绩报告可以选中行、全选进行删除操作\n · 如果用Ctrl+C复制成绩报告到Office中，请注意选择“选择性粘贴”的“Unicode文本”选项以防止乱码", "ℹ");
+		}
+	}
+}
